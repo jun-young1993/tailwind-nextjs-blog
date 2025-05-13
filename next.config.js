f4fc65de5cb7341ecfa0a7a73a4f86a191dda70d
junyoungkim <juny3738@gmail.com>
2025-05-13 14:36:43 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-
-
 const { withContentlayer } = require('next-contentlayer2')
-
-
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
